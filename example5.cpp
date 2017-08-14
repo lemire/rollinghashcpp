7af6ad9f78ff75e56ddad3d22ea3a72a564e4abd
Daniel Lemire <lemire@gmail.com>
2017-08-14 15:33:40 -0400

    Fixing example.

--- example5.cpp
+++ example5.cpp
@@ -4,7 +4,38 @@
 #include <iostream>
 #include "cyclichash.h"
 
+/*
+An issue is application-specific and has to do with the nature of DNA. Even
+though we usually represent DNA as a string of characters (such as `GATTACA`),
+this is really only half the story. DNA is double stranded with `A` pairing
+to `T` and `C` pairing to `G`, so the string `GATTACA` really represents the
+following molecule.
+
+```
+gattaca
+|||||||
+ɔʇɐɐʇƃʇ
+```
+
+In most contexts, we have no way of knowing whether the original piece of DNA
+sampled was from the top strand or the bottom strand, and so when we hash DNA
+sequences we typically want the two complementary sequences to hash to the
+same value.
+
+I used two cyclic hashes: one for the "top" strand of DNA (observed from the
+provided string, updated using forward updates) and one for the "bottom" strand
+(inferred from the provided string, updated using reverse updates). Then to get
+the hash for a particular k-mer (n-gram) in the DNA, I just XOR the current
+forward and reverse hashes.
+*/
+
+
 // Define DNA's complementary nucleotides
+//
+// Daniel: This is probably  inefficient. Needlessly so.
+// if efficiency matters, you want to define the character hash so that it takes the
+// key 'A' to the hash value of 'T' and so forth.
+//
 # define nucleotide_complement(ch) ( \
 	(toupper(ch)) == 'A' ? 'T' : \
 	(toupper(ch)) == 'T' ? 'A' : \
@@ -15,6 +46,7 @@
 // biologically identical and should hash to the same value. A sequence that is
 // equal to its reverse complement is a special case and should be handled
 // accordingly.
+//
 #define canonical_hash(fwd, rev) ( \
     fwd == rev ? rev : fwd ^ rev \
 )
@@ -24,46 +56,50 @@
 #define SEED2 1985
 #define HASHBITS 64
 
+
+// full string hash from scratch (for comparison)
+uint64_t fullhash(const string & input) {
+  assert(input.size() == WORDSIZE);
+    CyclicHash<uint64_t> forward(input.size(), SEED1, SEED2, HASHBITS);
+    CyclicHash<uint64_t> reverse(input.size(), SEED1, SEED2, HASHBITS);
+    for (int j = 0; j < input.size(); j++) {
+        forward.eat(input[j]);
+        reverse.eat(nucleotide_complement(input[input.size() - 1 - j]));
+    }
+    return canonical_hash(forward.hashvalue, reverse.hashvalue);
+}
+
+// check the rolling hash
+// k is the k-gram size, input is any string
+void demo(int k, string input) {
+    // Initialize the hash function to compute the hash of the first k-mer.
+    CyclicHash<uint64_t> forward(k, SEED1, SEED2, HASHBITS);
+    CyclicHash<uint64_t> reverse(k, SEED1, SEED2, HASHBITS);
+    for (int j = 0; j < k; j++) {
+        forward.eat(input[j]);
+        // going backward
+        reverse.eat(nucleotide_complement(input[k - 1 - j]));
+    }
+    // rolling has
+    uint64_t hashval = canonical_hash(forward.hashvalue, reverse.hashvalue);
+    assert(fullhash(input.substr(0,k)) == hashval);
+    std::cout <<  input.substr(0,k) << " " << hashval << std::endl;
+
+    for(int j = k ; j < input.size(); j++) {
+      forward.update(input[j-k], input[j]);
+      // note: you to flip the parameters of reverse_update
+      reverse.reverse_update(nucleotide_complement(input[j]), nucleotide_complement(input[j-k]));
+      // compute the rolling has
+      hashval = canonical_hash(forward.hashvalue, reverse.hashvalue);
+      // compare with full string hash
+      assert(fullhash(input.substr(j-k+1,k)) == hashval);
+      std::cout <<  input.substr(j-k+1,k) << " " << hashval << std::endl;
+    }
+}
+
 int main(int argc, char * argv[])
 {
-    // With word size k=5, a DNA sequence with 7 nucleotides has 3 k-mers.
-    string input = "GATTACA";
-    string kmer1 = "GATTA";
-    string kmer2 = "ATTAC";
-    string kmer3 = "TTACA";
-
-    // Initialize the hash function to compute the hash of the first k-mer.
-    CyclicHash<uint64_t> forward(WORDSIZE, SEED1, SEED2, HASHBITS);
-    CyclicHash<uint64_t> reverse(WORDSIZE, SEED1, SEED2, HASHBITS);
-    for (int j = 0; j < 4; j++) {
-        forward.eat(input[j]);
-        reverse.eat(nucleotide_complement(input[3 - j]));
-    }
-    uint64_t hashval = canonical_hash(forward.hashvalue, reverse.hashvalue);
-    std::cout << kmer1 << ' ' << hashval << '\n';
-
-    // Now do rolling updates to the hash.
-    forward.update('G', 'C');
-    reverse.reverse_update(nucleotide_complement('G'), nucleotide_complement('C'));
-    hashval = canonical_hash(forward.hashvalue, reverse.hashvalue);
-    std::cout << kmer2 << ' ' << hashval << '\n';
-
-    forward.update('A', 'A');
-    reverse.reverse_update(nucleotide_complement('A'), nucleotide_complement('A'));
-    hashval = canonical_hash(forward.hashvalue, reverse.hashvalue);
-    std::cout << kmer3 << ' ' << hashval << '\n';
-
-    // Finally, test that the rolling updates gives us the same hash as
-    // initializing the third k-mer from scratch.
-    CyclicHash<uint64_t> forward_test(WORDSIZE, SEED1, SEED2, HASHBITS);
-    CyclicHash<uint64_t> reverse_test(WORDSIZE, SEED1, SEED2, HASHBITS);
-    for (int j = 0; j < 4; j++) {
-        forward_test.eat(kmer3[j]);
-        reverse_test.eat(nucleotide_complement(kmer3[3 - j]));
-    }
-    uint64_t testhashval = canonical_hash(forward_test.hashvalue, reverse_test.hashvalue);
-    std::cout << "Test: " << hashval << " == " << testhashval << " ?\n";
-    assert(testhashval == hashval);
-
+    demo(5,"GATTACACAATAGCAAATT");
+    std::cout << " code looks good " << std::endl;
     return 0;
 }
